e26669a337bbdb7e1f9af86a31f8094886589db6
Ahmad Khaled <Ahmads20002000@gmail.com>
2025-01-27 21:04:18 +0200

    Add PasswordReset feature

---
+++ ProjectManagementSystem.Api/Features/Authentication/PasswordReset/PasswordReset/Command/PasswordResetCommand.cs
@@ -0,0 +1,58 @@
+﻿using HotelManagement.Service.PasswordHasherServices;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ProjectManagementSystem.Api.Entities;
+using ProjectManagementSystem.Api.Features.Common;
+using ProjectManagementSystem.Api.Features.Common.OTPService;
+using ProjectManagementSystem.Api.Repository;
+using ProjectManagementSystem.Api.Response.RequestResult;
+
+namespace ProjectManagementSystem.Api.Features.Authentication.PasswordReset.PasswordReset.Command;
+
+public record PasswordResetCommand(string Email, string NewPassword, string OTP) : IRequest<RequestResult<bool>>;
+
+public class PasswordResetCommandHandler : BaseRequestHandler<PasswordResetCommand, RequestResult<bool>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IOTPService _OTPService;
+
+    public PasswordResetCommandHandler(
+        BaseRequestHandlerParam requestHandlerParam,
+        IUnitOfWork unitOfWork,
+        IOTPService OTPService
+        )
+        : base(requestHandlerParam)
+    {
+        _unitOfWork = unitOfWork;
+        _OTPService = OTPService;
+    }
+
+    public override async Task<RequestResult<bool>> Handle(PasswordResetCommand request, CancellationToken cancellationToken)
+    {
+        var response = await ValidateRequest(request);
+        if (!response.IsSuccess)
+            return response;
+
+        var userid = await _unitOfWork.GetRepository<User>()
+            .GetAll(u => u.Email == request.Email)
+            .Select(u => u.Id)
+            .FirstOrDefaultAsync();
+
+        var newPassword = PasswordHasherService.HashPassord(request.NewPassword);
+        _unitOfWork.GetRepository<User>().SaveInclude(new User { Id = userid, Password = newPassword });
+
+        return RequestResult<bool>.Success(default, "Password reset successful");
+    }
+
+    private Task<RequestResult<bool>> ValidateRequest(PasswordResetCommand request)
+    {
+        var savedOTP = _OTPService.GetOTP(request.Email);
+
+        if (string.IsNullOrEmpty(savedOTP) || !savedOTP.Equals(request.OTP))
+        {
+            return Task.FromResult(RequestResult<bool>.Failure(default, "Invalid OTP"));
+        }
+
+        return Task.FromResult(RequestResult<bool>.Success(default, "Success"));
+    }
+}
---
+++ ProjectManagementSystem.Api/Features/Authentication/PasswordReset/PasswordResetEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectManagementSystem.Api.Features.Authentication.PasswordReset.PasswordReset.Command;
+using ProjectManagementSystem.Api.Features.Common;
+using ProjectManagementSystem.Api.Response.Endpint;
+
+namespace ProjectManagementSystem.Api.Features.Authentication.ResetPassword;
+
+[Route("api/auth/resetpassword/")]
+public class PasswordResetEndpoint : BaseEndpoint<PasswordResetViewModel, bool>
+{
+    public PasswordResetEndpoint(BaseEndpointParam<PasswordResetViewModel> param) : base(param)
+    {
+    }
+
+    [HttpPost]
+    public async Task<EndpointResponse<string>> PasswordReset([FromBody] PasswordResetViewModel param)
+    {
+        var command = new PasswordResetCommand(param.Email, param.NewPassword, param.OTP);
+        var result = await _mediator.Send(command);
+
+        return result.IsSuccess ? EndpointResponse<string>.Success(string.Empty, "Email Sent with OTP verification code")
+                         : EndpointResponse<string>.Failure(result.ErrorCode, result.Message);
+    }
+}
