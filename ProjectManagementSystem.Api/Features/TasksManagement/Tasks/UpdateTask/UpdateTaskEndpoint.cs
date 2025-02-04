0d4f2c4e3222391cc799b0ddc4a0d245c5cffe8a
minamichael14 <minamichael14@gmail.com>
2025-02-05 01:10:12 +0200

    Implementation of UpdateTaskDetails Feature

---
+++ ProjectManagementSystem.Api/Features/TasksManagement/Tasks/UpdateTask/UpdateTaskEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectManagementSystem.Api.Features.Common;
+using ProjectManagementSystem.Api.Features.TasksManagement.Tasks.UpdateTask.Commands;
+using ProjectManagementSystem.Api.Response.Endpint;
+
+namespace ProjectManagementSystem.Api.Features.TasksManagement.Tasks.UpdateTask
+{
+    [Route("api/Task")]
+
+    public class UpdateTaskEndpoint : BaseEndpoint<UpdateTaskRequestViewModel , EndpointResponse<bool>>
+    {
+        public UpdateTaskEndpoint(BaseEndpointParam<UpdateTaskRequestViewModel> param) : base(param)
+        {
+        }
+
+        [HttpPut]
+        public async Task<EndpointResponse<bool>> Update([FromBody] UpdateTaskRequestViewModel viewModel)
+        {
+            var result = await _mediator.Send(new UpdateTaskCommand(viewModel.TaskID, viewModel.Title, viewModel.Description, viewModel.Status, viewModel.UserID, viewModel.ProjectID));
+            return result.IsSuccess ? EndpointResponse<bool>.Success(true, "Success")
+                                    : EndpointResponse<bool>.Failure(result.ErrorCode, result.Message);
+        }
+
+    }
+}
