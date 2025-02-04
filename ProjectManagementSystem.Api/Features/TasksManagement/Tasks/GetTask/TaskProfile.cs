91a9f40409aaa18698b6a91569bb83084c1adfa7
Mohamed Shaqrani <mohamedshaqrani@gmail.com>
2025-02-04 21:03:48 +0200

    fix: Add AutoMapper mapping from ProjectTask to TaskDTO to resolve projection issue

---
+++ ProjectManagementSystem.Api/Features/TasksManagement/Tasks/GetTask/TaskProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ProjectManagementSystem.Api.Entities;
+using ProjectManagementSystem.Api.Features.TasksManagement.Tasks.GetTask;
+
+namespace ProjectManagementSystem.Api.Features.UserManagement.GetUsers;
+
+public class TaskProfile : Profile
+{
+    public TaskProfile()
+    {
+        CreateMap<ProjectTask, TaskDTO>()
+            .ForMember(dest => dest.ProjectName, opt => opt.MapFrom(src => src.Project.Title))
+            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User.Username));
+    }
+}
